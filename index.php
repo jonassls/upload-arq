e77ab7bf660baf20e35af21e7293d24586f0d8d5
Jonas <jonas.2022310916@aluno.iffar.edu.br>
2024-06-06 14:38:12 -0300

    1

--- index.php
+++ index.php
@@ -13,6 +13,18 @@
         <input type="file" name="arquivo"><br>
         <input type="submit" value="Enviar">
     </form>
+    <table>
+        <tbody>
+            <?php
+            foreach ($arquivo as $arquivo) {
+                echo "<tr><td>" . $arquivo['nome_arquivo'] . "</td>";
+                echo "<td><a href='alterar.php?nome_arquivo=".
+                $arquivo['Nome_arquivo']."'>Alterar</td>";
+                echo "<td><button>Excluir</button></td></tr>";
+            }
+            ?>
+        </tbody>
+    </table>
 </body>
 
 </html>
